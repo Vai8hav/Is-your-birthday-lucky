c9ceea1b2b38bcc7cba3e40284e03c6315ea7c18
Vai8hav <vaibhav.basantani@gmail.com>
2022-09-01 08:44:07 +0530

    refactor: removed comments
    These comments were for personal understanding which were removed

--- bday.js
+++ bday.js
@@ -6,7 +6,7 @@ const outputValue = document.querySelector("#output");
 checkButton.addEventListener("click", decisiveFunc)
 
 
-function addDateNumbers(dob) {  // adds up the digits of all the numbers in a birthdate
+function addDateNumbers(dob) { 
     let sum = 0;
     birthDate = dob.replaceAll("-","");
     for(let i=0; i<birthDate.length; i++) {
@@ -15,18 +15,18 @@ function addDateNumbers(dob) {  // adds up the digits of all the numbers in a bi
     return sum;
 }
 
-function decisiveFunc() {   // this function brings everything together which is then being called inside the EventListener.
+function decisiveFunc() {  
     const dob = dateOfBirth.value;
     const sum = addDateNumbers(dob);
 
-    if(sum&&luckyNumber.value)  // to check if both: the date and lucky number were entered. If yes, run the condition, if not, then print a mssg.
+    if(sum&&luckyNumber.value) 
     isYourBdayLucky(sum, luckyNumber.value);
 
     else
     outputValue.innerText = "Add all the values please";
 }
 
-function isYourBdayLucky(sum, luckyNumber) {   // to check if bday is lucky or not
+function isYourBdayLucky(sum, luckyNumber) {  
     if (sum % luckyNumber === 0) {
         outputValue.innerText = "Yahoo! You're lucky amigo! 🥳 ";
     }
